1148dbfee665d6af6dac2d0e5af523963fdbce91
David Ochoa <ochoa@ebi.ac.uk>
2015-03-21 20:01:02 +0000

    First commit

---
+++ init.el
@@ -0,0 +1,263 @@
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+(when (not package-archive-contents) (package-refresh-contents))
+
+(defvar my-packages '(
+                      auto-complete
+                      ac-cider
+                      better-defaults
+                      cider
+                      exec-path-from-shell
+                      find-file-in-project
+                      idle-highlight-mode
+                      ido-ubiquitous
+                      markdown-mode
+                      flycheck
+                      magit
+                      ; paredit
+                      r-autoyas
+                      smex
+                      ; smartparens
+                      scpaste
+                      yasnippet
+                      polymode
+                      ; writegood-mode
+		      textmate
+                      ))
+
+(let ((default-directory "~/.emacs.d/elpa/"))
+    (normal-top-level-add-to-load-path '("."))
+    (normal-top-level-add-subdirs-to-load-path))
+
+; install the missing packages
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+; Font
+(set-face-attribute 'default nil :foundry "apple" :family "Monaco")
+
+; Load theme
+(add-to-list 'load-path "~/.emacs.d/themes/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'tomorrow-night t)
+
+; Desactivate alarm
+(setq ring-bell-function 'ignore)
+
+; Prevent functions to access the clipboard
+(setq x-select-enable-clipboard nil)
+
+;Change windows
+(global-set-key [C-tab] 'other-window)
+
+; Yes or No in one character
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+; Get rid of temporary files
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+;smek provides the history on top of M-x
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+(require 'exec-path-from-shell)
+(when (memq window-system '(mac ns))
+      (exec-path-from-shell-initialize))
+
+(textmate-mode)
+
+; magit
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+; Ido to navigate the filesystem
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+; Line numbers
+(global-linum-mode 1)
+
+; Autocomplete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; turn on automatic bracket insertion by pairs. New in emacs 24
+(electric-pair-mode 1)
+
+; spell checker
+(setq flyspell-issue-welcome-flag nil)
+(if (eq system-type 'darwin)
+    (setq-default ispell-program-name "/usr/local/bin/aspell")
+  (setq-default ispell-program-name "/usr/bin/aspell"))
+(setq-default ispell-list-command "list")
+
+;; Allows the right alt key to work as alt
+(setq ns-right-alternate-modifier nil)
+
+; Some keybindings
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+; Indentation and spaces
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+; (setq-default show-trailing-whitespace t)
+
+; Markdown mode
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode t)
+            (writegood-mode t)
+            (flyspell-mode t)))
+(setq markdown-command "pandoc --smart -f markdown -t html")
+; (setq markdown-css-path (expand-file-name "markdown.css" abedra/vendor-dir))
+
+; Ruby
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (autopair-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+
+; YAML mode
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+;polymode
+(require 'poly-R)
+(require 'poly-markdown)
+
+;;; MARKDOWN
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+
+;;; R modes
+(add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+
+; ;; Load yasnippet
+; Next is necessary to install a list of default snippets
+; git clone --recursive https://github.com/capitaomorte/yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
+
+; (require 'r-autoyas)
+; (add hook 'ess-mode-hook 'r-autoyas-ess-activate)
+
+; ;;
+; ;; Load autocomplete
+; ;;
+; (require 'auto-complete)
+; (require 'auto-complete-config)
+; (global-auto-complete-mode t)
+; (add-to-list 'ac-dictionary-directories (expand-file-name "auto-complete" dotfiles-dir))
+; (setq ac-modes (append ac-modes '(org-mode)))
+; (ac-config-default)
+; (define-key ac-complete-mode-map [tab] 'ac-expand)
+; (setq ac-auto-start 4)
+; (ac-flyspell-workaround)
+; (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+; (define-key ac-completing-map (kbd "C-c h") 'ac-quick-help)
+
+
+
+;; Adapted with one minor change from Felipe Salazar at
+;; http://www.emacswiki.org/emacs/EmacsSpeaksStatistics
+
+(setq ess-ask-for-ess-directory nil)
+(setq ess-local-process-name "R")
+;;(setq inferior-R-program-name "/usr/local/bin/R")
+(setq ansi-color-for-comint-mode 'filter)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+(defun my-ess-start-R ()
+  (interactive)
+  (if (not (member "*R*" (mapcar (function buffer-name) (buffer-list))))
+      (progn
+        (delete-other-windows)
+        (setq w1 (selected-window))
+        (setq w1name (buffer-name))
+        (setq w2 (split-window w1 nil t))
+        (R)
+        (set-window-buffer w2 "*R*")
+        (set-window-buffer w1 w1name))))
+(defun my-ess-eval ()
+  (interactive)
+  (my-ess-start-R)
+  (if (and transient-mark-mode mark-active)
+      (call-interactively 'ess-eval-region)
+    (call-interactively 'ess-eval-line-and-step)))
+(add-hook 'ess-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+(add-hook 'inferior-ess-mode-hook
+          '(lambda()
+             (local-set-key [up] 'comint-previous-input)
+             (local-set-key [down] 'comint-next-input)))
+(add-hook 'Rnw-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+(require 'ess-site)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("645599a2aab022fd7677124515a3104a60ba64d2cafdd77a6e7703f8ae97250c" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" default)))
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
