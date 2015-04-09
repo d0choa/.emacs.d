64b58c393d8d0f4a18aa2ede52d3243ae75a03ba
David Ochoa <ochoa@ebi.ac.uk>
2015-04-09 21:57:29 +0100

    Markdown improved

--- init.el
+++ init.el
@@ -153,6 +153,13 @@
 (global-set-key [C-tab] 'other-window) ;Change windows
 
 ; Markdown mode
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
 (add-hook 'markdown-mode-hook
