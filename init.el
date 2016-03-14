de2cf5923ea0360fbcb6b3ab192f64962150749f
David Ochoa <ochoa@ebi.ac.uk>
2016-03-14 11:50:24 +0000

    Indent code when pasting

--- init.el
+++ init.el
@@ -186,6 +186,21 @@
 (electric-pair-mode 1)
 (electric-layout-mode 1)
 
+;; Indent code when pasting
+(dolist (command '(yank yank-pop))
+   (eval `(defadvice ,command (after indent-region activate)
+            (and (not current-prefix-arg)
+                 (member major-mode '(emacs-lisp-mode lisp-mode
+                                                      clojure-mode    scheme-mode
+                                                      haskell-mode    ruby-mode
+                                                      rspec-mode      python-mode
+                                                      c-mode          c++-mode
+                                                      objc-mode       latex-mode
+                                                      plain-tex-mode))
+                 (let ((mark-even-if-inactive transient-mark-mode))
+                   (indent-region (region-beginning) (region-end) nil))))))
+
+
 ; spell checker
 (setq flyspell-issue-welcome-flag nil)
 (if (eq system-type 'darwin)
